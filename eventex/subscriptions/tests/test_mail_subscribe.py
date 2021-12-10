58a063b2130c045e67afa0597da41fec22c4ff06
guilhermeviotti <guilhermeviotti@gmail.com>
2021-12-10 16:58:47 +0000

    instala django extensions

---
+++ eventex/subscriptions/tests/test_mail_subscribe.py
@@ -0,0 +1,38 @@
+from django.test import TestCase
+from django.core import mail
+
+
+class SubscribePostValid(TestCase):
+    """Valid POST should redirect to /inscricao/"""
+    def setUp(self):
+        data = dict(name='Henrique Bastos', cpf='12345678910',
+                    email='henrique@bastos.net', phone='21-99999-4444')
+        self.client.post('/inscricao/', data)
+        self.email = mail.outbox[0]
+
+    def test_subscription_email_subject(self):
+        expect = 'Confirmação de inscrição'
+
+        self.assertEqual(expect, self.email.subject)
+
+    def test_subscription_email_from(self):
+        expect = 'guilhermeviotti@gmail.com'
+
+        self.assertEqual(expect, self.email.from_email)
+
+    def test_subscription_email_to(self):
+        expect = ['guilhermeviotti@gmail.com', 'henrique@bastos.net']
+
+        self.assertEqual(expect, self.email.to)
+
+    def test_subscription_email_body(self):
+        contents = [
+            'Henrique Bastos',
+            '12345678910',
+            'henrique@bastos.net',
+            '21-99999-4444'
+        ]
+
+        for content in contents:
+            with self.subTest():
+                self.assertIn(content, self.email.body)
