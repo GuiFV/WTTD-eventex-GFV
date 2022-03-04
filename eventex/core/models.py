f61b80dc84e0feca09972bb8b24ee37e14237d41
guilhermeviotti <guilhermeviotti@gmail.com>
2022-03-04 11:48:32 -0300

    notas parciais - verificar models fixo

--- eventex/core/models.py
+++ eventex/core/models.py
@@ -1,4 +1,7 @@
 from django.db import models
 
 class Speaker():
-    pass
+    name = 'Grace Hopper'
+    website = 'http://hbn.link/hopper-site'
+    photo = 'http://hbn.link/hopper-pic'
+    description = 'Programadora e almirante.'
